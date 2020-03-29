7482e8fc2b7662d1b1b073107a1d2d3d5d31e30b
T4kano <edutakajr@gmail.com>
2020-03-29 03:43:43 -0300

    Funcionalidades avançadas

--- backend/tests/integration/ong.spec.js
+++ backend/tests/integration/ong.spec.js
@@ -1,7 +1,17 @@
 const request = require('supertest');
 const app = require('../../src/app');
+const connection = require('../../src/database/connection');
 
 describe('ONG', () => {
+  beforeEach(async () => {
+    await connection.migrate.rollback();
+    await connection.migrate.latest();
+  });
+
+  afterAll(async () => {
+    await connection.destroy();
+  });
+
   it('should be able to create a new ONG', async () => {
     const response = await request(app)
       .post('/ongs')
@@ -9,9 +19,11 @@ describe('ONG', () => {
         name: "SPAS",
         email: "ong@ong.com",
         whatsapp: "19971015718",
-        cidade: "Indaiatuba",
+        city: "Indaiatuba",
         uf: "SP"
       });
-      
+
+    expect(response.body).toHaveProperty('id');
+    expect(response.body.id).toHaveLength(8);
   });
 });
