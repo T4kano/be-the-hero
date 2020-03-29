b4261f9a140bfa0ce32d7b6112e0784546987655
T4kano <edutakajr@gmail.com>
2020-03-29 02:40:03 -0300

    Vulnerability fix 2

--- backend/tests/integration/ong.spec.js
+++ backend/tests/integration/ong.spec.js
@@ -1,5 +1,17 @@
+const request = require('supertest');
+const app = require('../../src/app');
+
 describe('ONG', () => {
-  it('should be able to create a new ONG', () => {
-    
+  it('should be able to create a new ONG', async () => {
+    const response = await request(app)
+      .post('/ongs')
+      .send({
+        name: "SPAS",
+        email: "ong@ong.com",
+        whatsapp: "19971015718",
+        cidade: "Indaiatuba",
+        uf: "SP"
+      });
+      
   });
 });
